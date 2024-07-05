f84bd3fdcf13c6d3a6891b3fe09cf780393b94e5
Uri Shaked <uri@urishaked.com>
2024-07-05 10:48:00 +0300

    fix: remove `-Wl,--import-memory` from build command
    Otherwise, chips with large static arrays fail to load, e.g. wokwi/wokwi-features#495

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p dist
-clang --target=wasm32-unknown-wasi --sysroot /opt/wasi-libc -nostartfiles -Wl,--import-memory -Wl,--export-table -Wl,--no-entry -Werror -o dist/chip.wasm $SOURCES
+clang --target=wasm32-unknown-wasi --sysroot /opt/wasi-libc -nostartfiles -Wl,--export-table -Wl,--no-entry -Werror -o dist/chip.wasm $SOURCES
